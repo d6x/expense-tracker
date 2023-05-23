9096dcc610f57cabf76e47a8a57097652d84953a
Liam Molloy <liam@Liams-Air.localdomain>
2023-05-24 00:24:23 +0100

    feat: added ability to delete transactions

--- expense-tracker/src/components/Transaction.js
+++ expense-tracker/src/components/Transaction.js
@@ -1,12 +1,15 @@
-import React from 'react'
+import React, { useContext} from 'react'
+import { GlobalContext } from '../context/GlobalState'
 
 export const Transaction = ({ transaction }) => {
+    const { deleteTransaction } = useContext(GlobalContext);
+
     const sign = transaction.amount < 0 ? '-' : '+';
 
     return (
     // if the transaction amount is less then 0, set the class to minus (red color), else set the class to plus (green) ? is IF : is ELSE!! 
     <li className={transaction.amount < 0 ? 'minus' : 'plus'}> 
-            {transaction.text} <span>{sign}{Math.abs(transaction.amount)}€</span><button className="delete-btn">x</button>
+            {transaction.text} <span>{sign}{transaction.amount}</span><button onClick={() => deleteTransaction(transaction.id)} className="delete-btn">x</button>
         </li>
     )
 }
